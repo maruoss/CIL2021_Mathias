69d90f95fec36f2c1b006778ef41981827a056e6
maruoss <mathias.ruoss@hotmail.com>
2021-06-16 00:44:45 +0200

    implemented transform function for train/ val

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # %%
 
 import os
+import PIL
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,19 +12,23 @@ from glob import glob
 import torch
 from torch import nn
 import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
 
 from PIL import Image
 from pathlib import Path
 import time
+from torchvision.transforms.functional_pil import resize
 from tqdm import tqdm
 
 from sklearn.model_selection import train_test_split
 
+import random
 
 # %%
 # Define random seeds
 random_seed = 343
 torch.manual_seed(17)
+# random.seed(1222)
 
 # Define root to current working directory + "/Data"
 root = Path.cwd() / 'Data'
@@ -45,18 +50,23 @@ image_paths = get_filenames_of_path(root / "training" / "training" / "images")
 groundtruth_paths = get_filenames_of_path(root / "training" / "training" / "groundtruth")
 
 # %%
-# Image Transform function
-transform_fn = transforms.Compose(
-    [transforms.ToTensor(),
-    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)) # Normalize for deeplabv3
-    ])
+# # Image Transform function
+# transform_fn = transforms.Compose(
+#     [
+#     # transforms.Resize(256), # has to be multiple of 16 for patch acc. metric
+#     # transforms.CenterCrop(224),
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)) # Normalize needed for deeplabv3
+#     ])
 
-# Target Transform function
-target_transform_fn = transforms.Compose(
-    [
-    transforms.ToTensor(),
-    # transforms.Normalize((0.5), (0.5)) # Normalize to [-1, 1]
-    ])
+# # Target Transform function
+# target_transform_fn = transforms.Compose(
+#     [
+#     # transforms.Resize(256), # has to be multiple of 16 for patch acc. metric
+#     # transforms.CenterCrop(224),
+#     transforms.ToTensor(),
+#     # transforms.Normalize((0.5), (0.5)) # Normalize to [-1, 1]
+#     ])
 
 
 
@@ -75,36 +85,125 @@ assert [y[-7:] for y in [str(x) for x in val_image_paths]] == [y[-7:] for y in [
 # %%
 # Create custom Dataset class to load one sample of tuple (image, groundtruth)
 class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self, image_paths: list, groundtruth_paths: list, device: str, transforms=None, target_transforms=None):
+    def __init__(self, image_paths: list, groundtruth_paths: list, device: str, train=True, resize_to=(400, 400)): #RESIZE TO?
         # Initialize imagepaths, grountruthpaths and transform
         self.image_paths = image_paths
         self.groundtruth_paths = groundtruth_paths
-        self.transforms = transforms
-        self.target_transforms = target_transforms
-        self.device = device
+        self.device = device # CPU or cuda?
+        self.train = train # Train or validation mode?
+        self.resize_to = resize_to
 
+    def _my_segmentation_transforms(self, image : PIL or torch.tensor, segmentation, train=True): # _ for function inside class
+        """preprocessing for image, segmentation in training:
+        -> Input: PIL or tensor, Output: PIL or tensor"""
+
+        img_size = TF._get_image_size(image) # Get Size of Input Image
+        
+        # Validation Augmentation
+        # if not train:
+            # 1. Resizing
+            # size = 256 # Adjust
+            # image = TF.resize(image, size)
+            # segmentation = TF.resize(segmentation, size)
+
+            # # 2. Crop
+            # out_size = self.resize_to # Adjust
+            # image = TF.center_crop(image, out_size)
+            # segmentation = TF.center_crop(segmentation, out_size)
+
+        # Training Augmentation
+        if train:
+            # 1. Random resize crop
+            top = random.randint(0, img_size[1]-200) # -100 so that not (400, 400) chosen as left upper coordinate
+            left = random.randint(0, img_size[1]-200) # -100 so that not (400, 400) chosen as left upper coordinate
+            height = random.randint(200, img_size[1])
+            width = random.randint(200, img_size[1])
+            size = self.resize_to
+
+            image = TF.resized_crop(image, top=top, left=left, height=height, width=width, size=size)
+            segmentation = TF.resized_crop(segmentation, top=top, left=left, height=height, width=width, size=size)
+
+            # 2. Rotation
+            if random.random() > 0.5: # Adjust
+                angle = random.randint(-180, 180) # Adjust
+                image = TF.rotate(image, angle)
+                segmentation = TF.rotate(segmentation, angle)
+            
+            # 3. Horizontal flip
+            if random.random() > 0.5: # Adjust prob.
+                image = TF.hflip(image)
+                segmentation = TF.hflip(segmentation)
+
+            # 4. Random Grayscale
+            if random.random() > 0.5:
+                image = TF.rgb_to_grayscale(image, num_output_channels=3)
+                # not needed for segmentation mask        
+
+            # 5. Gaussian Blur
+            if random.random() > 0.5:
+                sigma = random.uniform(0.1, 2.)
+                kernel_size = random.randrange(3, 50, 2)
+                image = TF.gaussian_blur(image, kernel_size=kernel_size, sigma=sigma)
+                # not needed for segmentation mask
+
+            # 6. ColorJitter: Adjust brightness, contrast, saturation, hue
+            if random.random() > 0.5:
+                brightness = random.uniform(0.5, 2)
+                image = TF.adjust_brightness(image, brightness_factor=brightness)
+
+                contrast = random.uniform(0.5, 2)
+                image = TF.adjust_contrast(image, contrast_factor=contrast)
+
+                saturation = random.uniform(0.5, 2)
+                image = TF.adjust_saturation(image, saturation_factor=saturation)
+
+                hue = random.uniform(-0.3, 0.3)
+                image = TF.adjust_hue(image, hue_factor=hue)
+                #not needed for segmentation
+            
+            # 7. Sharpness
+            if random.random() > 0.5:
+                sharpness = random.uniform(0.5, 2)
+                image = TF.adjust_sharpness(image, sharpness_factor=sharpness)
+                # not needed for segmentation
+
+            # 8. Equalize
+            if random.random() > 0.5:
+                image = TF.equalize(image)
+                # not needed for segementation
+
+        # 3. ToTensor
+        image = TF.to_tensor(image)
+        segmentation = TF.to_tensor(segmentation)
+
+        # 4. Normalize
+        image = TF.normalize(image, (0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        # not needed for segmentation
+
+        return image, segmentation
+
+     
     def __getitem__(self, idx: int):
 
-        # Load image and groundtruth: sample path, open via PIL and convert to ndarray, float32 for tensors
-        im = np.array(Image.open(self.image_paths[idx]))
-        gt = np.array(Image.open(self.groundtruth_paths[idx]))
+        # Numpy not necessary, gives errors on transforms. ToTensor transforms at the end transforms to tensor anyway.
+        im = Image.open(self.image_paths[idx])
+        gt = Image.open(self.groundtruth_paths[idx])
 
-        # Move axis: convert HWC into CHW, PyTorch needs C, H, W
-        # im = np.moveaxis(im, -1, 0) # not needed with ToTensor() self.transforms
+        # Apply first transformation function to images and label
+        if self.train: # If train == True
+            im, gt = self._my_segmentation_transforms(im, gt, train=True)
+        else: # Validation phase
+            im, gt = self._my_segmentation_transforms(im, gt, train=False)
 
-        # Transform to tensor, attach to device
+        # Finish transformation: ToTensor, normalize, attach to device
         # Images
-        if self.transforms:
-            im = self.transforms(im).to(self.device)
+        # if self.transforms:
+        #     im = self.transforms(im).to(self.device)
+        # # Targets
+        # if self.target_transforms:
+        #     gt = self.target_transforms(gt).to(self.device)
 
-        # Targets
-        if self.target_transforms:
-            gt = self.target_transforms(gt).to(self.device)
-        # else:
-        #     gt = torch.from_numpy(gt).to(self.device)
-        #     gt = gt.unsqueeze(0) / 255. # add channel dimension of 1
-
-        return im, gt
+        return im.pin_memory().to(device=self.device, non_blocking=True), gt.pin_memory().to(device=self.device, non_blocking=True)
 
     def __len__(self):
         return len(self.image_paths)
@@ -116,12 +215,12 @@ class CustomDataset(torch.utils.data.Dataset):
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Instantiate Datasets for train and validation
-train_dataset = CustomDataset(train_image_paths, train_groundtruth_paths, device, transform_fn, target_transforms=target_transform_fn)
-val_dataset = CustomDataset(val_image_paths, val_groundtruth_paths, device, transform_fn, target_transforms=target_transform_fn)
+train_dataset = CustomDataset(train_image_paths, train_groundtruth_paths, device, train=True) # train=True
+val_dataset = CustomDataset(val_image_paths, val_groundtruth_paths, device, train=False) # train=False
 
 # Instantiate Loaders for these datasets, # SET BATCH SIZE HERE
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=8, shuffle=True)
-val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=8, shuffle=True)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=8, shuffle=True) # BATCH SIZE
+val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=8, shuffle=True) # BATCH SIZE
 
 
 # %%
@@ -143,18 +242,49 @@ plt.show()
 
 # %%
 
-# 400  // 16
+# Test for image transformations:
+image = torch.from_numpy(np.moveaxis(np.array(Image.open(image_paths[0])), -1, 0))
+# label = torch.unsqueeze(torch.from_numpy(np.array(Image.open(groundtruth_paths[0]))), dim=0)
+
+print(image.shape)
+
+# print(label.shape)
+
+# transform = transforms.Compose([transforms.RandomRotation(90)])
+# image = transform(image)
+
+# image = transforms.functional.adjust_brightness(image, brightness_factor=3.)
+# image = transforms.functional.adjust_contrast(image, contrast_factor=3)
+# image = transforms.functional.adjust_hue(image, hue_factor=0.5)
+# image = transforms.functional.adjust_saturation(image, saturation_factor=3)
+# image = transforms.functional.adjust_sharpness(image, sharpness_factor=5)
+# image = transforms.functional.equalize(image)
+
+# image = transforms.functional.resized_crop(image, 0, 0, 200, 200, (400, 400))
+# image = transforms.functional.center_crop(image, (224, 224))
+
+plt.imshow(torch.moveaxis(image.cpu(), 0, -1))
+
+print(image)
 
 # %%
-a = train_labels.reshape(-1, 25, 16, 25, 16).mean((-1, -3)) > 0.25
 
-b = train_labels.reshape(-1, 25, 16, 25, 16).mean((-1, -3)) > 0.25
+a = Image.open(image_paths[0])
+# print(len(a))
 
-(a == b).float().mean()
+a = TF._get_image_size(a)
 
+a
 
 # %%
 
+import random
+# random.seed(11)
+# a = random.randrange(3, 4)
+# b = random.uniform(2, 20)
+
+
+# %%
 # class FirstNet(nn.Module):
 #     def __init__(self):
 #         super().__init__()
@@ -167,12 +297,10 @@ b = train_labels.reshape(-1, 25, 16, 25, 16).mean((-1, -3)) > 0.25
 #             nn.Linear(512, 1),
 #             # nn.Sigmoid() # use Sigmoid + BCELoss, or BCEwithLogitLoss without Sigmoid -> numerical stability (logsumexptrick)
 #         )
-
 #     def forward(self, x):
 #         x = self.flatten(x)
 #         logits = self.linear_relu_stack(x)
 #         return logits
-
 
 # %%
 # from torchinfo import summary ->have to install with Pip, ideally after every other necessary package is installed with conda
@@ -336,14 +464,14 @@ def train_epoch(train_dataloader, eval_dataloader, model, loss_fn, metric_fns, o
     plt.legend()
     plt.show()
 
-    # Show plots for all additional metrics
-    for k, _ in metric_fns.items():
-        plt.plot([v[k] for _, v in history.items()], label='Training '+k)
-        plt.plot([v["val_"+k] for _, v in history.items()], label='Validation '+k)
-        plt.ylabel(k)
-        plt.xlabel('Epochs')
-        plt.legend()
-        plt.show()
+    # Show plots for all additional metrics # UNCOMMENT/ COMMENT OUT
+    # for k, _ in metric_fns.items():
+    #     plt.plot([v[k] for _, v in history.items()], label='Training '+k)
+    #     plt.plot([v["val_"+k] for _, v in history.items()], label='Validation '+k)
+    #     plt.ylabel(k)
+    #     plt.xlabel('Epochs')
+    #     plt.legend()
+    #     plt.show()
 
 
 # Metric functions
@@ -354,9 +482,10 @@ def accuracy_fn(y_hat, y):
 
 def patch_accuracy(y_hat, y):
     # computes accuracy weighted by patches
-    h_patches = y.shape[-2] // PATCH_SIZE # Global variable
+    h_patches = y.shape[-2] // PATCH_SIZE # Number of patches in the height
     w_patches = y.shape[-1] // PATCH_SIZE
 
+    # Reshape to patches x patchsize to take mean across patchsize 
     patches_hat = y_hat.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean((-1, -3)) > CUTOFF
     patches = y.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean((-1, -3)) > CUTOFF
 
@@ -364,10 +493,11 @@ def patch_accuracy(y_hat, y):
 
 
 
-# %%
-metric_fns = {'acc': accuracy_fn}
 
 # %%
+# Define global variables
+PATCH_SIZE = 16
+CUTOFF = 0.25
 
 # Instantiate model
 model = createDeepLabHead() # function that loads pretrained deeplabv3 and changes classifier head
@@ -381,41 +511,26 @@ for x in model.backbone.parameters():
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 # Define loss function, BCEWithLogitLoss -> needs no sigmoid layer in neural net (num. stability)
 loss_fn = nn.BCEWithLogitsLoss()
-
+# Define metrics
+metric_fns = {'acc': accuracy_fn, "patch_acc": patch_accuracy}
 
 # %%
 # Train
 train_epoch(train_dataloader, eval_dataloader=val_dataloader, model=model, loss_fn=loss_fn, 
-             metric_fns=metric_fns, optimizer=optimizer, n_epochs=2)
+             metric_fns=metric_fns, optimizer=optimizer, n_epochs=30)
 
 # %%
 
-dict1 = {"test": 1, "train": 2}
+# dict1 = {"test": 1, "train": 2}
 
-# %%
-for k, v in dict1.items():
-    print(k, v)
+# # %%
+# for k, v in dict1.items():
+#     print(k, v)
 
 
-
-# %% 
-
-test(2)
 
 # *****************************************************************************************************
 # %%
-from torch import nn
-a = nn.BCEWithLogitsLoss()
-
-
-#%%
-
-str(a)
-
-# %%
-
-if str(a) == "BCEWithLogitsLoss":
-    print("yes")
 
 
 # %%
@@ -541,8 +656,6 @@ b = torch.from_numpy(a)
 
 
 # %%
-
-b.float()
 
 
 # %%
