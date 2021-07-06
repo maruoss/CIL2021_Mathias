bc042ed7c00c52fd0b182c7f16ab1f5de38c07a1
maruoss <mathias.ruoss@hotmail.com>
2021-07-06 12:09:56 +0200

    upd leonh

--- main_leonhard.py
+++ main_leonhard.py
@@ -187,7 +187,7 @@ N_EPOCHS = 100
 # %% ************************* START CASCADING **********************************
 # CASCADE TRAINS 1*************************************
 # Set title for tensorboard
-cascade_title = "_cascade10b"
+cascade_title = "_cascade10"
 # ***************************************************
 name_model = str(model)[:3]
 name_loss = str(loss_fn)[:7]
