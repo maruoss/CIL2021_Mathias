8e642750bb77f99de8d0a322abaa9ffce63b9320
maruoss <mathias.ruoss@hotmail.com>
2021-07-06 16:26:31 +0200

    casc1

--- main_leonhard.py
+++ main_leonhard.py
@@ -187,7 +187,7 @@ N_EPOCHS = 100
 # %% ************************* START CASCADING **********************************
 # CASCADE TRAINS 1*************************************
 # Set title for tensorboard
-cascade_title = "_cascade13"
+cascade_title = "_cascade1"
 # ***************************************************
 name_model = str(model)[:3]
 name_loss = str(loss_fn)[:7]
@@ -270,7 +270,7 @@ val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE,
 LEARNING_RATE = 0.0001
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 # NUM EPOCHS
-N_EPOCHS = 300
+N_EPOCHS = 500
 
 # Comments
 name_loss = str(loss_fn)[:7]
